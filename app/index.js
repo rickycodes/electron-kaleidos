47d6b7b187466160ac6b594ce1e59088835f1425
Ricky Miller <ricky.miller@gmail.com>
2015-09-23 04:27:36 -0400

    Remove config from node_modules

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const config = require('config')
+const config = require('./config.js')
 
 const kaleidoscope = require('kaleidoscope')(config)
 const scene = require('scene')(config)
