2c8bfcca76ad65d962a700cba9aefd837c0d521d
Ricky Miller <ricky.miller@gmail.com>
2015-10-28 19:36:19 -0500

    Tidy things up

--- main.js
+++ main.js
@@ -9,17 +9,17 @@ require('crash-reporter').start()
 var mainWindow = null
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
+app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform != 'darwin') {
+  if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-app.on('ready', function() {
+app.on('ready', function () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
@@ -30,7 +30,7 @@ app.on('ready', function() {
   mainWindow.openDevTools()
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
