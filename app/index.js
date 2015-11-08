3c213440c5ba906b6b0e79bdabcfbaeffb04da38
Ricky Miller <ricky.miller@gmail.com>
2015-11-08 02:29:19 -0500

    Move audio to spectrum-analyzer

--- app/index.js
+++ app/index.js
@@ -2,21 +2,7 @@
 
 const config = require('./config.js')
 const sa = require('spectrum-analyzer')
-const audio = document.createElement('audio')
-const analyzer = new sa(32 * 0.5, 0.80)
-
-audio.crossOrigin = 'anonymous'
-audio.src = config.track
-analyzer.setSource(audio)
-
-audio.currentTime = 0
-audio.play()
-
-audio.addEventListener('ended', function (e) {
-  // console.log('loop!')
-  this.currentTime = 0
-  this.play()
-})
+const analyzer = new sa(config.track, 32 * 0.5, 0.80)
 
 require('kaleidoscope')(config, analyzer)
 require('scene')(config, analyzer)
