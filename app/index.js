7504c1b6776172372ac537bd9f213b32d6b245b0
Ricky Miller <ricky.miller@gmail.com>
2015-11-10 21:55:07 -0500

    Increase binCount, use all spectrum data

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@
 
 const config = require('./config.js')
 const sa = require('spectrum-analyzer')
-const analyzer = new sa(config.track, 32 * 0.5, 0.80)
+const analyzer = new sa(config.track, 32, 0.80)
 
 require('kaleidoscope')(config, analyzer)
 require('scene')(config, analyzer)
--- main.js
+++ main.js
@@ -34,6 +34,7 @@ app.on('ready', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
+    app.quit()
     mainWindow = null
   })
 })
